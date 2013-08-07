e99bba8fd90a479337512d5f43e523303cda8a7e
Flowck II <flowck96@hotmail.com>
2013-08-07 18:25:58 +0100

    grunt file

--- gruntfile.js
+++ gruntfile.js
@@ -1,7 +1,12 @@
 module.exports = function(grunt){
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
+		pkg: grunt.file.readJSON('package.json')
 		// tasks
-		
+		/*min : {
+			dist: {
+				src: ['src/documents/moment.js'],
+				dest: 'out/moment.min.js'
+			}
+		}*/
 	});
 }
